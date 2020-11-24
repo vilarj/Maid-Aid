07ff9d2499baae22cab9d8e85204da635bd273a1
Vilarj <lamaravillasan@outlook.com>
2020-11-24 12:16:35 -0400

    Working on the bar menu

--- maid-aid/src/App.js
+++ maid-aid/src/App.js
@@ -1,19 +1,54 @@
+import React, {useState} from 'react';
 
 function App() {
   return (
     <div className="App">
     {/*Todo esto es parte del titulo */}
-    <h1>Maid-Aid</h1>
-    <h3>Registrate</h3>
-    <h3>Ayuda</h3>
-    <h3>Hazte un Socio</h3>
-    <h3>Todos los Servicios</h3>
-    <h3>Categorias de Servicios</h3>
-    <h3>Sobre Nosotros</h3>
+    <Navbar>
+      <NavItem icon="🤓" />
+      <NavItem icon="🤓" />
+      <NavItem icon="🤓" />
+      <NavItem icon="🤓" />
 
 
+      <NavItem icon="🤓" />
+      <NavItem icon="🤓" />
+    </Navbar>
+
+{/*
+      <ul>Maid-Aid</ul>
+        <li>Registrate</li>
+        <li>Ayuda</li>
+        <li>Hazte un Socio</li>
+        <li>Todos los Servicios</li>
+        <li>Categorias de Servicios</li>
+        <li>Sobre Nosotros</li>
+      </ul>
+*/}
     </div>
   );
+}
+
+function Navbar (props) {
+    return (
+      <nav className='navbar'>
+        <ul className='navbar-nav'> {props.children} </ul>
+      </nav>
+    );
+}
+
+function NavItem (props) {
+  const [open, setOpen] = useState(false);
+
+    return (
+      <li className='nav-item'>
+          <a href='#' className='icon-button'onClick={() => setOpen(!open)}>
+            {props.icon}
+          </a>
+
+          {open && props.chidlren}
+      </li>
+    );
 }
 
 export default App;
